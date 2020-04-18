5e2bbf5fbe28bcc8c17a33d9b5fd970de90dd023
Md Sazid Khan <mdsazidkhan18@gmail.com>
2020-04-19 03:05:59 +0530

    First commit

---
+++ src/test/java/com/lowesforgeeks/eventshostingplatform/EventshostingplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lowesforgeeks.eventshostingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventshostingplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
