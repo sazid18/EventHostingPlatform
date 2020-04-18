1bf270d9b5bcb53f1b995f94d94bd97e0b5a6714
Md Sazid Khan <mdsazidkhan18@gmail.com>
2020-04-19 04:09:42 +0530

    member pojo is created

---
+++ eventshostingplatform/EventshostingplatformApplication.java
@@ -0,0 +1,13 @@
+package com.lowesforgeeks.eventshostingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventshostingplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventshostingplatformApplication.class, args);
+	}
+
+}
